d1a4243504b2f5cc61986042357d6644f0c173df
Sateoo <matteo.salvi@studenti.ittsrimini.edu.it>
2023-03-16 09:25:57 +0100

    Prima versione

---
+++ salvi.matteo.4h.Persone/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using salvi.matteo._4h.Persone.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace salvi.matteo._4h.Persone
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ATA a = new ATA();
+            Console.WriteLine(a);
+        }
+        private void PulsanteApri(object sender, RoutedEventArgs e)
+        {
+            ATA a= new ATA();
+            Console.WriteLine("Sono io :"+a);
+            /*try
+            {
+                dgDati.ItemsSource = new Utenti("utenti.csv");
+            }
+            catch (Exception errore)
+            {
+                MessageBox.Show(errore.Message);
+            }*/
+        }
+        private void dgDati_LoadingRow(object sender, DataGridRowEventArgs e)
+        {
+            /*try
+            {
+                if (e != null && e.Row != null)
+                {
+                    Persona u = e.Row.Item as Persona;
+                    if (u != null)
+                    {
+                        if (u.Tipo == "Alunno")
+                        {
+                            e.Row.Background = Brushes.LightGreen;
+                            e.Row.Foreground = Brushes.Black;
+                        }
+                        else if (u.Tipo == "Docente")
+                        {
+                            e.Row.Background= Brushes.LightBlue;
+                            e.Row.Foreground = Brushes.Black;
+                        }
+                    }
+                }
+            }
+            catch (Exception errore)
+            {
+                MessageBox.Show(errore.Message);
+            }*/
+        }
+    }
+}
---
+++ salvi.matteo.4h.Persone/Models/Persona.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace salvi.matteo._4h.Persone.Models
+{
+    public enum TipoPersona { NonDefinito,ATA,Alunno,Docente}
+    public class Persona
+    {
+        public TipoPersona Tipo { get; set; }
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public string Email { get; set; }
+        
+        /*public Persona() { }
+        public Persona(string str) 
+        {
+            string[] colonne = str.Split(";");
+            Tipo = colonne[0];
+            Nome = colonne[1];
+            Cognome = colonne[2];
+            Email = colonne[3];
+        }*/
+    }
+    public class ATA : Persona 
+    {
+        public ATA() 
+        {
+            Tipo = TipoPersona.ATA;
+        }
+    }
+    /*internal class Utenti : List<Persona>
+    {
+        public Utenti(string filename)
+        {
+
+            StreamReader fin = new StreamReader(filename);
+            fin.ReadLine();
+            while (!fin.EndOfStream)
+            {
+                string str = fin.ReadLine();
+                Add(new Persona(str));
+            }
+            fin.Close();
+
+
+        }
+    }*/
+}
